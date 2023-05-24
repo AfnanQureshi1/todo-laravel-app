88edf5247ff6894a66b261a8c102d6fd77230a9d
Muhammad Arsalan <50544594+arslangzr@users.noreply.github.com>
2023-05-24 12:25:18 +0500

    Added factory and seeders

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Todo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ */
+class TodoFactory extends Factory
+{
+        /**
+
+     * The name of the factory's corresponding model.
+
+     *
+
+     * @var string
+
+     */
+
+     protected $model = Todo::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title'=>$this->faker->paragraph
+        ];
+    }
+}
